2cfb332c2df3428927ffa198aca69dae43e5a37d
GOVARDHANREDDY <gund22030.it@rmkec.ac.in>
2026-03-11 14:54:40 +0530

    Update for Render deployment

--- app.py
+++ app.py
@@ -560,5 +560,6 @@ def clear_history():
 
 
 if __name__ == '__main__':
-     app.run(host="127.0.0.1", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
 
